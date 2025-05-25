96dfdd1ffc104c4b3c1e05993951b2a2b5b5d0a4
NILANK NIKHIL <38290758+nilankh@users.noreply.github.com>
2025-05-26 01:51:42 +0530

    Update main.go

--- main.go
+++ main.go
@@ -14,14 +14,15 @@ type Simple struct {
 }
 
 func SimpleFactory(host string) Simple {
-    return Simple{"Hello", "Dear Students", host}
+    return Simple{"Hello", "World", host}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    // simple := Simple{"Hello", "Dear Students", r.Host}
     simple := SimpleFactory(r.Host)
 
     jsonOutput, _ := json.Marshal(simple)
+
+    w.Header().Set("Content-Type", "application/json")
 
     fmt.Fprintln(w, string(jsonOutput))
 }
